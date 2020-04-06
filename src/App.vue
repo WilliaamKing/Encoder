f32d060987f06293759e0922d4a1ed72b60d9f11
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-06 23:02:56 +0300

    update (App.vue): styles for v-authentication-form

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,6 @@ export default {
 </script>
 
 <style>
-  body{
-    width: 100vw;
-    height: 100vh;
-    margin: 0;
-    padding: 0;
-  }
-
   #app {
     display: flex;
     justify-content: center;
@@ -28,7 +21,16 @@ export default {
     background: linear-gradient(159deg, rgba(0,159,194,1) 0%, rgba(13,10,11,1) 100%);
   }
 
-  .v-authentication-form{
+  .v-authentication-form {
     align-self: center;
+    margin: 20px;
+  }
+
+  .v-authentication-form /deep/ header h2{
+      font: 400 56px 'Permanent Marker', sans-serif;;
+  }
+
+  .v-authentication-form /deep/ form p{
+     font: 400 12px 'Arial', sans-serif;
   }
 </style>
