13d87aadc32ff69cf9669b57776ad637a0bd6d2e
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-06 10:24:53 +0300

    add (App.vue): connect vue routing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
@@ -18,9 +19,16 @@ export default {
   }
 
   #app {
+    display: flex;
+    justify-content: center;
+    align-content: space-between;
     width: 100%;
     height: 100%;
     background: rgb(0,159,194);
     background: linear-gradient(159deg, rgba(0,159,194,1) 0%, rgba(13,10,11,1) 100%);
+  }
+
+  .v-authentication-form{
+    align-self: center;
   }
 </style>
