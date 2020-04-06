696a105409c10fdde9214a1e2bece210d7ac5ba1
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-06 07:49:38 +0300

    add (main.js): connect vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import VAuthenticationForm from './components/v-authentication-form/index';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: VAuthenticationForm
+  }
+];
+
+const router = new VueRouter({routes});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
