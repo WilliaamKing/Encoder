2d4be554c2ab9264609b43379c0ea13c30e351c9
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-07 11:51:58 +0300

    add (component/registration-form): markup and styles for the component

---
+++ src/components/registration-form/index.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="registrartion-form form">
+        <header>
+            <h2>Registarion</h2>
+        </header>
+
+        <form>
+            <label for="userName"></label>
+            <input id="userName" type="name" autocomplete="off"  placeholder="Username"/>
+
+            <label for="userEmail"></label>
+            <input id="userEmail" type="email" autocomplete="off"  placeholder="Email"/>
+
+            <label for="userPassword"></label>
+            <input id="userPassword" type="password" autocomplete="off" placeholder="Password"/>
+
+            <button>Register</button> 
+
+             <div class='additional-actions'>
+                <router-link to='/'>Back</router-link>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default{}
+</script>
+
+<style scoped>
+    .registrartion-form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        width: 550px;
+        height: 500px;
+        color: #cbced0;
+    }
+
+    header h2{
+        margin: 15px;
+    }
+
+    form {
+        width: 100%;
+    }
+
+    label {
+        position: relative;
+    }
+
+    label::before {
+        position: absolute;
+        transform: translateY(50%);
+        width: 20px;
+        height: 20px;
+        content: '';
+    }
+
+    label:nth-of-type(1)::before {
+        background: url('../../common/media/images/user-icon.png') center/contain no-repeat;
+    }
+
+    label:nth-of-type(2)::before {
+        background: url('../../common/media/images/email-icon.png') center/contain no-repeat;
+    }
+
+    label:nth-of-type(3)::before {
+        background: url('../../common/media/images/password-icon.png') center/contain no-repeat;
+    }
+
+    input {
+        display: block;
+        width: 100%;
+        height: 40px;
+        box-sizing: border-box;
+        padding-left: 40px;
+        margin-bottom: 10px;
+        font-size: 14px;
+        background: none;
+        border: none;
+        outline: none;
+        border-bottom: 1px solid #cbced0;
+        color: #cbced0;
+    }
+
+    input::-webkit-input-placeholder {
+        color: #cbced0;
+    }
+
+    button {
+        width: 100%;
+        height: 42px;
+        margin: 15px 0;
+        background: #16a086;
+        color: #cbced0;
+        border: none;
+        outline: none;
+    }
+
+    form a {
+       text-align: center;
+       text-decoration: none;
+       color: #cbced0;
+       cursor: pointer;
+    }
+
+    .additional-actions {
+        display: flex;
+        justify-content: space-evenly;
+    }
+</style>
