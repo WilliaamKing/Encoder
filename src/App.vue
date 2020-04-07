f2bf1a01517216c5b64e3e4ebf172f975d04e636
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-07 10:58:55 +0300

    add (App.vue): animation for routes and update classes name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name='fade'>
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -33,7 +35,8 @@ export default {
   methods: {
      setCurrentPath (newPath){
         const actualPath = {};
-        actualPath.path = (this.currentUser.userName !== '') ? newPath : '/';
+        const newPathConditions = [this.currentUser.userName !== '', newPath === '/registration', newPath === '/restore-password'];
+        actualPath.path = (newPathConditions.some(el => el)) ? newPath : '/';
 
         if(actualPath.path !== this.$route.path){
           this.$router.push(actualPath);
@@ -55,16 +58,25 @@ export default {
     background: linear-gradient(159deg, rgba(0,159,194,1) 0%, rgba(13,10,11,1) 100%);
   }
 
-  .v-authentication-form {
+  .authentication-form {
     align-self: center;
     margin: 20px;
   }
 
-  .v-authentication-form /deep/ header h2{
+  .authentication-form /deep/ header h2{
       font: 400 56px 'Permanent Marker', sans-serif;;
   }
 
-  .v-authentication-form /deep/ form p{
+  .authentication-form /deep/ form a,
+  .authentication-form /deep/ button{
      font: 400 12px 'Arial', sans-serif;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1.5s;
+  }
+
+  .fade-enter, .fade-leave-to{
+    opacity: 0;
   }
 </style>
