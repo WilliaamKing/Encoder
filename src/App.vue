0d9e6a4c1248300d0f2f119a5ae12f4a11e057a9
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-09 00:09:47 +0300

    add (App.vue): functionality for login and registration

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
     <transition name='fade'>
-      <router-view></router-view>
+      <router-view @authentication-error='showMessage'></router-view>
     </transition>
   </div>
 </template>
 
 <script>
 import {store} from './store/index';
+import {mapState, mapActions} from 'vuex';
 
 export default {
   name: 'App',
   computed:{
-    currentUser(){
-      return store.state.currentUser;
-    },
+    ...mapState(['currentUser', 'users']),
     currentRoute(){
       return this.$route.path;
     }
@@ -24,23 +23,48 @@ export default {
         this.setCurrentPath(newPath);
     },
     currentUser(){
+      this.updateLocalStorage();
       this.currentUser.name !== '' ? this.$router.push('/') : this.$router.push('/login');
+    },
+    users(){
+      this.updateLocalStorage();
     }
   },
   created(){
-      this.$router.push({
-          path: '/home'
-      });
+      this.uploadStore();
+     this.setCurrentPath('/');
   },
   methods: {
+     ...mapActions(['setCurrentUser', 'addUser']),
      setCurrentPath (newPath){
         const actualPath = {};
-        const newPathConditions = [this.currentUser.userName !== '', newPath === '/registration'];
-        actualPath.path = (newPathConditions.some(el => el)) ? newPath : '/login';
+        const newFirstPathConditions = [this.currentUser.name !== '', newPath === '/registration'];
+        actualPath.path = (newFirstPathConditions.some(el => el)) ? newPath : '/login';
+
+        if (this.currentUser !== '' && (newPath === '/login' || newPath === '/registration'))
+            actualPath.path = '/';
 
         if(actualPath.path !== this.$route.path){
-          this.$router.push(actualPath);
+            this.$router.push(actualPath);
         }    
+     },
+     uploadStore(){
+        const defaultUser = { name: '',  password: '', historyOfCoding: []};
+
+        if (localStorage.getItem('encoder-users') === null) {
+           localStorage.setItem('encoder-users', JSON.stringify({ currentUser: defaultUser, users: []}));
+        }
+
+        const ENCODER_USERS = JSON.parse(localStorage.getItem('encoder-users'));
+        this.setCurrentUser(ENCODER_USERS.currentUser);
+
+        ENCODER_USERS.users.map((el)=> this.addUser(el));
+     },
+     showMessage(message){
+        alert(message);
+     },
+     updateLocalStorage(){
+       localStorage.setItem('encoder-users', JSON.stringify(this.$store.state));
      }
   },
   store
