7c23c3360afaf9e409562ecbc00b30179f1a4600
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-08 18:39:06 +0300

    refactor (main.js): remove route's code

--- src/main.js
+++ src/main.js
@@ -1,32 +1,10 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
-import VAuthenticationForm from './components/v-authentication-form/index.vue';
-import VRegistrationForm from './components/v-registration-form/index.vue';
-import VRestoreForm from './components/v-restore-form/index.vue';
+import {router} from './router/index';
 import {connectStyle} from './common/connectStyleScript';
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter);
-
 connectStyle();
-
-const routes = [
-  {
-    path: '/login',
-    component: VAuthenticationForm
-  },
-  {
-    path: '/registration',
-    component: VRegistrationForm
-  },
-  {
-    path: '/restore-password',
-    component: VRestoreForm
-  }
-];
-
-const router = new VueRouter({routes});
 
 new Vue({
   router,
