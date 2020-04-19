b80033eb2805eee5d5f0370c64813e8cbce10b55
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-19 16:22:31 +0300

    add (components/v-strart-page): base markup for start page

---
+++ src/components/v-start-page/index.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="v-start-page">
+        <header v-html="welcomeMessage"></header>
+   </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+    name: 'v-start-page',
+    computed: {
+        ...mapState(['currentUser']),
+        welcomeMessage (){
+            return `<h2>Hello ${this.currentUser.name}</h2>
+                    <p>What do want to do today?</p>`;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .v-start-page {
+        transform: translateY(75px);
+        max-width: 1024px;
+        width: 100%;
+    }
+
+    header {
+        font-family: "Fira Code", monospace;
+        font-weight: 500;
+        color: #ffffff;
+    }
+</style>
