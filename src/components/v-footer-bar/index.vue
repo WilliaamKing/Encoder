3ac877d747d98b3daed1bade265cd7a66714d7fa
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-21 19:56:29 +0300

    add (v-footer-bar): markup for footer bar

---
+++ src/components/v-footer-bar/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <footer class="v-footer-bar">
+        <a class="git-section" href="https://github.com/WilliaamKing/Encoder" target="_blank">
+            <v-icon>mdi-github</v-icon>
+            <p>Encoder</p>
+        </a>
+
+        <p>Developed by Dmitriy Shmaliuk</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "v-footer-bar"
+}
+</script>
+
+<style lang="scss" scoped>
+    .v-footer-bar {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        max-width: 1024px;
+        height: 50px;
+        margin: 20px auto;
+        color: #ffffff;
+    }
+
+    .git-section {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        font: 400 24px "Fira Code", sans-serif;
+
+        & .v-icon, &:visited {
+            color: #ffffff;
+            margin-right: 10px;
+        }
+    }
+</style>
