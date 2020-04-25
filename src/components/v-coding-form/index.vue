b62a2579d6e86d83f7b939664430101520460808
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-04-25 18:54:13 +0300

    add (v-coding-form): animation and update layout

--- src/components/v-coding-form/index.vue
+++ src/components/v-coding-form/index.vue
@@ -5,12 +5,27 @@
         </header>
 
          <v-select :items="algorithms"
-                   color="#ffffff"
-                   :label="selectLabel">
+                   :label="selectLabel"
+                   color="#ffffff">
         </v-select>
 
-        <v-encoder-textarea label="Encoding text"></v-encoder-textarea>
-        <v-encoder-textarea label="Decoding text"></v-encoder-textarea>
+        <v-icon class="swap" @click="flip">mdi-swap-vertical</v-icon>
+
+        <div class="coding-inputs">
+            <v-encoder-textarea :class="{active: isActive.encode, disactive: !isActive.encode}"
+                                id="encodingInput"
+                                :readonly="!isActive.encode"
+                                ref="encodingInput" 
+                                label="Encoding text">
+            </v-encoder-textarea>
+
+            <v-encoder-textarea :class="{active: isActive.decode, disactive: !isActive.decode}" 
+                                id="decodingInput"
+                                :readonly="!isActive.decode"
+                                ref="decodingInput"
+                                label="Decoding text">
+            </v-encoder-textarea>
+        </div>
     </div>
 </template>
 
@@ -35,8 +50,31 @@ export default {
         name (){
             return (this.$route.path + ' form').slice(1);
         },
+        isActive (){
+            return {
+                encode: this.$route.path !== '/decode',
+                decode: this.$route.path === '/decode',
+            }
+        },
         selectLabel (){
             return `Alogorithm of ${this.$route.path.slice(1, this.$route.path.length - 1)}ing`;
+        }
+    },
+    methods: {
+        flip (){
+            const newPath = this.$route.path === '/encode' ? 'decode' : 'encode';
+            this.$router.push(newPath);
+            this.changeTranslateText();
+        },
+        changeTranslateText(){
+            if (this.isActive.encode) {
+                this.$refs.encodingInput.value = this.$refs.decodingInput.value;
+                this.$refs.decodingInput.value = '';
+            }
+            else {
+                this.$refs.decodingInput.value = this.$refs.encodingInput.value;
+                this.$refs.encodingInput.value = '';
+            }
         }
     },
     components: {
@@ -47,6 +85,7 @@ export default {
 
 <style lang="scss">
     .v-coding-form {
+        position: relative;
         width: 100%;
         max-width: 1024px;
 
@@ -94,6 +133,59 @@ export default {
                 top: 33px !important;
                 left: 0px !important;
             }
+        }
+        
+        & .coding-inputs {
+            position: relative;
+            width: 100%;
+            height: 450px;
+
+            & .v-encoder-textarea {
+                position: absolute;
+                width: 100%;
+                transition: top 1s linear;
+
+                &.active {
+                    top: 0px;
+                    animation: rescale 1s both;
+                }
+
+                &.disactive {
+                    top: 226px;
+                    animation: scale 1s both;
+                }
+
+                & textarea {
+                    font-family: 'Fira Code', sans-serif;
+                }
+            }
+        }
+
+        & .swap {
+            position: absolute;
+            top: 103px;
+            right: 10px;
+            z-index: 3;
+        }
+    }
+
+    @keyframes scale {
+        0%, 100% {
+            transform: scale(1.0);
+            z-index: 2;
+        }
+        50% {
+            transform: scale(1.04);
+        }
+    }
+
+     @keyframes rescale {
+        0%, 100% {
+            transform: scale(1.0);
+            z-index: 1;
+        }
+        50% {
+            transform: scale(0.96);
         }
     }
 </style>
