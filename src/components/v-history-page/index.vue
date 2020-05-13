402ed11075678c784bd393b8df1a48cc4e00db05
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-05-13 18:56:56 +0300

    add (v-history-page): base layout for page

---
+++ src/components/v-history-page/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="v-history-page">
+       <header>
+            <h2>History</h2>
+        </header>
+
+        <v-history-card></v-history-card>
+    </div>
+</template>
+
+<script>
+import VHistoryCard from '../v-history-card/index.vue';
+
+export default {
+    name: 'v-history-page',
+    components: {
+        VHistoryCard
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .v-history-page {
+        width: 100%;
+    }
+
+    header {
+        margin-bottom: 10px;
+        font: 400 24px 'Fira Code', sans-serif;
+        text-align: center;
+        color: #ffffff;
+
+        & h2 {
+            font-weight: 400;
+        }
+
+        & h2:first-letter {
+            text-transform: uppercase;
+        }
+    }
+</style>
