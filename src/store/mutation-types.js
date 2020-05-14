377a82d30a86a35950e3952bb91adf3c714143e1
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-05-14 07:03:00 +0300

    add (store/mutation-type.js): ADD_RECCORD_TO_HISTORY

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -2,3 +2,4 @@ export const SET_CURRENT_USER = 'setCurrentUser';
 export const DELETE_CURRENT_USER = 'deleteCurrentUser';
 export const SET_ERROR = 'setError';
 export const ADD_USER = 'addUser';
+export const ADD_RECCORD_TO_HISTORY = 'addReccordToHistory';
